35e454cb9909efe955d222a8f7f64f36519b1769
monaiem2022 <95927663+monaiem2022@users.noreply.github.com>
2023-07-27 01:14:40 +0300

    Add files via upload

--- cadbury.js
+++ cadbury.js
@@ -1,17 +1,30 @@
 
-var body = document.getElementById('body');
+// var body = document.getElementById('body');
 
-var me = screen.availWidth;
+// var me = screen.availWidth;
 
 
-body.onload = function () {
-    if(me>769) {
+// body.onload = function () {
+//     if(me>769) {
        
-        body.style.display = 'none';
+//         body.style.display = 'none';
        
-    }  else {
-        body.style.display = 'block'
-    }
-}
+//     }  else {
+//         body.style.display = 'block'
+//     }
+// }
 
 
+
+
+function sendToWhatsApp() {
+    const imageURL = document.getElementById("image").src;
+    const text = "This is the text to send to WhatsApp";
+  
+    // Copy the image to the clipboard
+    navigator.clipboard.writeText(imageURL);
+  
+    // Open WhatsApp with the text and image
+    const whatsappURL = "whatsapp://send?text=" + encodeURIComponent(text) + "&image=" + encodeURIComponent(imageURL);
+    window.open(whatsappURL);
+  }
